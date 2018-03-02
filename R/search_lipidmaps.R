1971516157bab2ff67e426866e1dcf887d2ec4f2
wilsontom <wilsontom89@gmail.com>
2018-03-02 19:01:44 +0000

    fix output for single row returns

---
+++ R/search_lipidmaps.R
@@ -0,0 +1,42 @@
+#' Search LIPID MAPS
+#'
+#'
+#' @param type a character string indicating
+#' @param value
+#'
+#' @return a `tibble` containing the following fields for each returned entry
+#'        - __name__ Systematic name
+#'        - __id__ LIPID MAPS database id
+#'        - __mz__ Monoisotopic accurate mass
+#'        - __inchi__ InChi Code
+#'        - __inchi_key__ InChiKey
+#'        - __smiles__ Smiles
+#'        - __formula__ Molecular formula
+#'        - __core_class__
+#'        - __main_class__
+#'        - __sub_class__
+#'
+#'
+#' @export
+
+search_lipidmaps <- function(type, value)
+{
+  RESTURL <- 'http://www.lipidmaps.org/rest/compound'
+
+  url_request <-  paste0(RESTURL, '/', type, '/', value, '/all/')
+
+  lm_res <- httr::GET(url_request) %>% httr::content(., 'parsed')
+
+  if(length(grep('Row', names(lm_res))) >= 1){
+    lm_parse <-
+      purrr::map(lm_res, ~ {
+        t(unlist(.)) %>% as_tibble()
+      }) %>% bind_rows()
+  }else{
+    lm_parse <- as_tibble(lm_res)
+  }
+
+
+  return(lm_parse)
+
+}
