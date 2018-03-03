28afc8cc0bb6e14f8e8e99b01b69f402572bd636
wilsontom <wilsontom89@gmail.com>
2018-03-03 23:29:36 +0000

    formatting

--- R/search_by_mass.R
+++ R/search_by_mass.R
@@ -11,15 +11,16 @@
 #' @examples
 
 search_by_mass <- function(mz, adduct, ppm = 1.0)
-  {
-
+{
   RESTURL <- 'http://www.lipidmaps.org/rest/moverz/LIPIDS/'
 
-  query <- paste0(RESTURL, mz, '/', adduct,'/', ppm, 'txt')
+  query <- paste0(RESTURL, mz, '/', adduct, '/', ppm, '/txt')
 
-  mz_res <- hlm_res <- httr::GET(query) %>% httr::content(., 'parsed')
+  mz_res <-
+    hlm_res <- httr::GET(query) %>% httr::content(., 'parsed')
 
-  mz_res_clean<- mz_res %>% str_split('\n') %>% .[[1]]  %>% str_split('\t')
+  mz_res_clean <-
+    mz_res %>% str_split('\n') %>% .[[1]]  %>% str_split('\t')
 
   len <- purrr:::map(mz_res_clean, ~ {
     length(.) < 9
@@ -30,11 +31,24 @@ search_by_mass <- function(mz, adduct, ppm = 1.0)
   mz_res_clean2 <- mz_res_clean[-bool]
 
   mz_tib <- purrr::map(mz_res_clean2,
-                  ~{tibble::tibble(field = mz_res_clean2[[1]],
-                                                  value = .[1:9]) %>%
-                      tidyr::spread(field,value)}) %>% bind_rows() %>% select(.,'Input m/z', 'Matched m/z', Delta, Name, Formula, Ion, Category, 'Main class', 'Sub class')
+                       ~ {
+                         tibble::tibble(field = mz_res_clean2[[1]],
+                                        value = .[1:9]) %>%
+                           tidyr::spread(field, value)
+                       }) %>% bind_rows() %>% select(
+                         .,
+                         'Input m/z',
+                         'Matched m/z',
+                         Delta,
+                         Name,
+                         Formula,
+                         Ion,
+                         Category,
+                         'Main class',
+                         'Sub class'
+                       )
 
-  mz_tib <- mz_tib[-1,]
+  mz_tib <- mz_tib[-1, ]
 
   mz_tib$`Input m/z` <- as.numeric(mz_tib$`Input m/z`)
   mz_tib$`Matched m/z` <- as.numeric(mz_tib$`Matched m/z`)
@@ -44,7 +58,3 @@ search_by_mass <- function(mz, adduct, ppm = 1.0)
 
   return(mz_tib)
 }
-
-
-
-
